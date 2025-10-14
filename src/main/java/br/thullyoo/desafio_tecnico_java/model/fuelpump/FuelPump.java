2c559aac00d4278620e0a84c3a03a567ae8a9ba2
thullyo <thullyofilho@gmail.com>
2025-10-14 13:55:34 -0300

    feat: add FuelPump class

---
+++ src/main/java/br/thullyoo/desafio_tecnico_java/model/fuelpump/FuelPump.java
@@ -0,0 +1,51 @@
+package br.thullyoo.desafio_tecnico_java.model.fuelpump;
+
+import br.thullyoo.desafio_tecnico_java.model.fueltype.FuelType;
+import jakarta.persistence.*;
+
+@Table(name = "TB_FUEL_PUMP")
+@Entity
+public class FuelPump {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "fuel_type_id", nullable = false)
+    private FuelType fuelType;
+
+    public FuelPump() {
+    }
+
+    public FuelPump(String name, FuelType fuelType) {
+        this.name = name;
+        this.fuelType = fuelType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+}
