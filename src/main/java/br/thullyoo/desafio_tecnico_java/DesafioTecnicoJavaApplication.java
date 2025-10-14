86b0703ee95545f3a59a2da634059e7b486c9141
thullyo <thullyofilho@gmail.com>
2025-10-14 13:48:25 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/desafio_tecnico_java/DesafioTecnicoJavaApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.desafio_tecnico_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoJavaApplication.class, args);
+	}
+
+}
